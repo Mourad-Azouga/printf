e78982ee48bdcd872938f554f289ccf3e05ba71c
Mourad-Azouga <mourad-azouga@outlook.fr>
2023-07-24 08:15:10 -0700

    Made some changes here and there for better readablity no script changes, please test.

--- functions.c
+++ functions.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
-
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -19,7 +17,6 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -74,7 +71,6 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -141,7 +137,6 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
--- get_size.c
+++ get_size.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * get_size - Calculates the size to cast args
+ * @format: Formatted string in which to print the arg
+ * @i: arg list
+ * Return: size
  */
 int get_size(const char *format, int *i)
 {
